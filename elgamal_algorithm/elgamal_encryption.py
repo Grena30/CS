6ef3f98dbcb82b35a7c4eaff0a7c3f9d57ae1959
Grena30 <grenadinu@yahoo.com>
2023-10-02 21:21:30 +0300

    feat: add elgamal algorithm

---
+++ elgamal_algorithm/elgamal_encryption.py
@@ -0,0 +1,31 @@
+import random
+
+
+def compute_beta_generator(prime_number, generator, private_key):
+    beta_a = pow(generator, private_key, prime_number)
+
+    return beta_a
+
+
+def encrypt(msg, generator, beta_generator, prime_number):
+    k = random.randint(2, prime_number - 2)
+    r = pow(generator, k, prime_number)
+
+    new_msg = []
+    for i in range(len(msg)):
+        t = pow(beta_generator, k, prime_number)
+        t = (t * msg[i]) % prime_number
+        new_msg.append(t)
+
+    return r, new_msg
+
+
+def decrypt(t, r, private_key, prime_number):
+
+    r = pow(r, -private_key, prime_number)
+    new_msg = []
+
+    for i in range(len(t)):
+        new_msg.append((r * t[i]) % prime_number)
+
+    return new_msg
---
+++ elgamal_algorithm/main.py
@@ -0,0 +1,4 @@
+from utils import enter_menu
+
+if __name__ == '__main__':
+    enter_menu()
